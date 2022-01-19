9e9e875353f950fbc40a483019aea9cd6c853c38
Ruben Espinosa <ruben.espinosa@snyk.io>
2022-01-19 18:16:08 +0100

    Add dynamic filepath

--- validator.sh
+++ validator.sh
@@ -3,12 +3,16 @@
 read_file()
 {
   invalid=()
-  file=".env"
+  file="$FILEPATH"
+  if [ ! -f "$file" ]; then
+    echo "ERROR! $file file does not exist."
+    exit 1
+  fi
   while IFS="=" read -r key value; do
     [ -z "$value" ] && invalid+=("$key")
   done < "$file"
   if (( ${#invalid[@]} )); then
-    echo "ERROR!! There are some invalid environment variables:"
+    echo "ERROR! There are some invalid environment variables:"
     printf '%s\n' "${invalid[@]}"
     exit 1
   fi
